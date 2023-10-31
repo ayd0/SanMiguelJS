641f8f5601a32771c6e4975edbe5bb315d803a87
Cameron Dockan <127259937+CameronDockan@users.noreply.github.com>
2023-10-31 01:01:35 -0700

    created enemy "ramGirl"
    created enemy called ramGirl. ramGirl is its own class and is instantiated inside the Game class constructor().

--- brains.js
+++ brains.js
@@ -13,6 +13,9 @@ window.addEventListener('load', function() {
     const dojoSprite = new Image()
     dojoSprite.src = 'assets/dojox2.png'
 
+    const ramGirlSprite = new Image()
+    ramGirlSprite.src = 'assets/ramGirl_x1.png'
+
     class HardBoundaries {
         constructor (game) {
             this.game = game;
@@ -228,6 +231,87 @@ window.addEventListener('load', function() {
         }
     }
 
+    class RamGirl {
+        constructor(game) {
+            this.game = game;
+            this.w = 64;
+            this.h = 64;
+            this.x = 300;
+            this.y = 300;
+            this.frameX = 0;
+            this.frameY = 0;
+            this.moveRight = true;
+            this.moveLeft = false;
+            this.moveUp = false;
+            this.moveDown = false;
+            this.speed = 1;
+            this.walk = false;
+            this.attack = true;
+        }
+        draw (ctx) {
+            ctx.drawImage(ramGirlSprite, this.w*this.frameX, this.h*this.frameY, this.w, this.h, this.x, this.y, this.w, this.h)
+        }
+        update () {
+            const ramDirection = () => {
+                if (this.moveRight && this.moveUp || 
+                    this.moveRight && this.moveDown ||
+                    this.moveRight) {
+                        this.frameY = 1;
+                }
+                if (this.moveLeft && this.moveUp ||
+                    this.moveLeft && this.moveDown ||
+                    this.moveLeft) {
+                        this.frameY = 0
+                }
+                if (this.moveUp &&
+                    !this.moveLeft &&
+                    !this.moveRight) {
+                        this.frameY = 0;
+                }
+                if (this.moveDown &&
+                    !this.moveLeft &&
+                    !this.moveRight) {
+                        this.frameY = 1
+                    }
+            }
+            ramDirection();
+
+            const ramMovement = () => {
+                if (this.moveRight) this.x += this.speed;
+                if (this.moveLeft) this.x -= this.speed;
+                if (this.moveUp) this.y -= this.speed;
+                if (this.moveDown) this.y += this.speed;
+            }
+            ramMovement();
+
+            const ramWalk = () => {
+                if (this.walk) {
+                    if (gameFrame % 10 === 0) {
+                        if(this.frameX < 4) {
+                            this.frameX ++;} 
+                        else {this.frameX = 1}
+                    }
+                }
+                if (!this.walk && !this.attack) {
+                    this.frameX = 0;
+                }
+            }
+            ramWalk();
+
+            const ramAttack = () => {
+                if (this.attack) {
+                    if (gameFrame % 10 === 0) {
+                        if(this.frameX < 7) {
+                            this.frameX ++;} 
+                        else {this.frameX = 7}
+                    }
+                }
+            }
+            ramAttack()
+
+        }
+    }
+
     class Player {
         constructor(game) {
             this.game = game;
@@ -421,6 +505,7 @@ window.addEventListener('load', function() {
             this.hardBoundaries = new HardBoundaries(this);
             this.scrollBoundaries = new ScrollBoundaries(this);
             this.player = new Player(this);
+            this.ramGirl = new RamGirl(this);
             this.input = new InputHandler(this);
             this.keys = [];
 
@@ -428,12 +513,14 @@ window.addEventListener('load', function() {
         update() {
             this.dojoLevel.update()
             this.scrollBoundaries.update()
+            this.ramGirl.update()
             this.player.update()
         }
         draw(ctx) {
             this.dojoLevel.draw(ctx);
             this.hardBoundaries.draw(ctx);
             // this.scrollBoundaries.draw(ctx);
+            this.ramGirl.draw(ctx);
             this.player.draw(ctx);
         }
     }
@@ -471,6 +558,6 @@ window.addEventListener('load', function() {
     }
 
     animate(0);
-    
+
 
 })
